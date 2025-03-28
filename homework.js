55df5315db9277801dedb918545380d0032dc11b
Nino-Osadze <ninoosadze00@gmail.com>
2025-03-28 18:15:40 +0400

    add 2nd task

--- homework.js
+++ homework.js
@@ -1,4 +1,14 @@
 // 1st task 
-let a = 87
+/*let a = 87
 let b = 65
-console.log(a+b)
+console.log(a+b)*/
+
+//2nd task 
+let student = {
+    firstname: "Nino",
+    lastname: "osadze",
+    age: 79,
+    email: "ninobla@gmail.com",
+    marks: 10,
+}
+console.log(student.firstname)
